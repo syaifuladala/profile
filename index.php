6c363e8f391f0b2f384fa48337eac2a3154e82a8
syaifuladala <adalasyaiful@gmail.com>
2020-06-23 15:19:54 +0700

    first

---
+++ index.php
@@ -0,0 +1,105 @@
+<html>
+    <head>
+        <title>Syaiful Adala - Profile</title>
+        <link rel="stylesheet" href="maincss.css" type="text/css">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+            <div class="menu">
+                <ul>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </header>
+        
+        <div class="container">
+            <center>
+                <div class="profile">
+                    <img class="imgprofile" src="assets/fotoprofile.png">
+                    <h1>Syaiful Adala</h1>
+                    <h4>Informatics Engineering UPN "Veteran" Yogyakarta</h4>
+                </div>
+            </center>
+
+            <div class="info-content">
+                <div class="about">
+                    <h2>About</h2>
+                    <p>Experienced Web Developer with a demonstrated history of working in the information
+                        technology and services industry. Skilled in User Experience (UX), PHP, User Interface
+                        Design, Graphic Design, and English. Strong engineering professional with a Bachelor's
+                        degree focused in Information Technology from Universitas Pembangunan Nasional Veteran Yogyakarta. 
+                    </p>
+                </div>
+                
+                <div class="basicinfo">
+                    <h2>Basic Information</h2>
+                    <table width="100%" border="0" class="tabelbasicinfo" align="center">
+                        <tr>
+                            <td align="left" width="40%">Address</td>
+                            <td width="60%">Yogyakarta, Indonesia</td>
+                        </tr>
+                        <tr>
+                            <td align="left" width="40%">Place of Birth</td>
+                            <td width="60%">Blora</td>
+                        </tr>
+                        <tr>
+                            <td align="left" width="40%">Date of Birth</td>
+                            <td width="60%">7 March 1998</td>
+                        </tr>
+                        <tr>
+                            <td align="left" width="40%">Gender</td>
+                            <td width="60%">Male</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+
+            <div class="skills-content">
+                <div class="skillsilustration">
+                    <img class="imgskillsilustration" src="assets/ilustration-skills.png">
+                </div>
+                <div class="skills">
+                    <h2>Skills</h2>
+                    <div class="computing"><span>Web Development</span></div>
+                    <div class="design"><span>Design Graphics</span></div>
+                    <div class="marketing"><span>Marketing</span></div>
+                </div>
+            </div>
+
+            <div class="form">
+                <h2>Business Form</h2>    
+                    <table>
+                        <tr>
+                            <td><p>E-mail</p></td>
+                        </tr>
+                        <tr>
+                            <td><input class="form-input" type="email"></td>
+                        </tr>
+                        <tr>
+                            <td><p>Subject</p></td>
+                        </tr>
+                        <tr>
+                            <td><input class="form-input" type="text"></td>
+                        </tr>
+                        <tr>
+                            <td><p>Message</p></td>
+                        </tr>
+                        <tr>
+                            <td><textarea></textarea></td>
+                        </tr>
+                        <tr>
+                            <td><button>Send</button></td>
+                        </tr>
+                    </table>
+            </div>
+        </div>
+        <footer>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=adalasyaiful@gmail.com&su=Hai_Syaiful">
+            <img class="imgfooter" src="assets/logo-mail.png"></a>
+            <a href="https://linkedin.com/in/syaifuladala"><img class="imgfooter" src="assets/logo-linkedin.png"></a>
+            <a href="https://github.com/syaifuladala"><img class="imgfooter" src="assets/logo-github.png"></a>
+        </footer>
+    </body>
+</html>
